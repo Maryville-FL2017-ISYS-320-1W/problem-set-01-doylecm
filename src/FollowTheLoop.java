28d83234013dd7bf6abca196ee10aac094bb9ab0
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-26 10:22:41 -0600

    Finished

--- src/FollowTheLoop.java
+++ src/FollowTheLoop.java
@@ -8,16 +8,24 @@
 
 1. Fill out the table for the loop below
 
-| ITERATION 		 | VARIABLE NAME	 | VARIABLE NAME	 | VARIABLE NAME	 |
-| initialization	 |				 |				 |				 | 
-| 1				 |				 |				 |				 |
+| ITERATION 		 | VARIABLE NAME | VARIABLE NAME |  VARIABLE NAME |
+| initialization	 |	   MAX		 |     number    |      count     | 
+|				     |   product	 |	   number    |			      |
+| int count = MAX, which is 5 and is greater than or equal to 1, making the 
+| statement true. The product is equal to the count (5) times 2 minus 1, which equals 9.
+|The number is equal to int number + the product (9), which is 9. We then repeat this 
+|same pattern descending from 5 until we reach 0, which would result in a false statement,
+|completing the loop. This results in the final number being (9 + 7 + 5 + 3 + 1 = 25).
+|Then we print "The result is:" + the number on the output line
+|and we get:
+|The result is: 25
 
 
 */
 
 public class FollowTheLoop {
 
-	   public static final int MAX = 4;
+	   public static final int MAX = 5;
 	   public static void loopDeDoop() {
 	      int number = 0;
 	      for( int count = MAX; count >= 1; count-- ) {
