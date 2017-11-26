392c2e809e260f2aa72f66cd6c33e550b5bc4c4c
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-26 09:18:12 -0600

    Finished

--- src/ScalableNumbersRight.java
+++ src/ScalableNumbersRight.java
@@ -7,9 +7,10 @@
 public class ScalableNumbersRight {
 
 	public static void main(String[] args) {
-		for( int line = 1; line <= 5; line++ ) {
-			for( int spacesIndex = 1; spacesIndex <= 5 - line; spacesIndex++ ) {
-				System.out.print(" ");
+		final int TOP_SCALE = 10;
+			for( int line = 1; line <= TOP_SCALE; line++ ) {
+				for( int spacesIndex = 1; spacesIndex <= TOP_SCALE - line; spacesIndex++ ) {
+					System.out.print(" ");
 			}
 			for( int numbersIndex = 1; numbersIndex <= line; numbersIndex++ ) {
 				System.out.print( line );
