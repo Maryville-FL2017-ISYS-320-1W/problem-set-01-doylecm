a769aecb1ce3276c050660e350b8453db1d3fb95
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-26 08:22:32 -0600

    Finished

--- src/NestedLoopBox.java
+++ src/NestedLoopBox.java
@@ -5,14 +5,21 @@
 */
 
 /* 4. Your pseudocode algorithm here
-
-
+* for (each of six lines)
+* 		for (each of six times)
+* 			write an asterisk
 */
 public class NestedLoopBox {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			for (int line = 1; line <= 6; line++) { //number of times to repeat
+				for (int i = 1; i <= 6; i++) { // number of times to write
+					System.out.print("*"); // write asterisks
+
+				}
+				System.out.println(" "); // blank output line
 
 	}
 
-}
+}}
