e0104605275b1060cd55b5c59c32f516f3180af1
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-26 09:18:36 -0600

    Finished

--- src/DescendingSquares.java
+++ src/DescendingSquares.java
@@ -5,15 +5,25 @@
 */
 
 /* 2. Your pseudocode algorithm here
-
-
+ *  for (each of ten integers, starting with ten and descending to 1)
+ *  multiply integer times itself
+ *  write final results and add a space between each result
 */
 
 public class DescendingSquares {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		printSumOfSquares();
+		}
+		
+		public static void printSumOfSquares() {
+			
+			for( int i = 10; i >= 1; i-- ) { // number of integers 
+				int finalResult = i * i;
+			    System.out.print(finalResult + " ");
+			
+		}
 	}
-
 }
+	
+
